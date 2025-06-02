e442ebe8a29a774413e71fd75a715ef6ab8a239e
baptistamiguel <miguel03baptista@gmail.com>
2025-06-02 10:45:31 -0300

    Primeira versão da terceira página.

---
+++ app/src/main/java/com/example/indoorar/activity_criar.kt
@@ -0,0 +1,21 @@
+package com.example.indoorar
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.seuapp.innovationar.R
+
+class activity_criar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_criar)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}

d3c20b7271f5451f9dc4fc430a87ad2a7a4e6c83
Rafael Favetta <rafaelfavetta@gmail.com>
2025-07-15 10:47:58 -0300

    também adicionando o google-services

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,10 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+
+    plugins {
+        id("com.google.gms.google-services") version "4.4.3"
+    }
 }
 
 dependencyResolutionManagement {

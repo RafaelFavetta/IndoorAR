fa32749d93c426c976059722eb606cb035412ed9
baptistamiguel <miguel03baptista@gmail.com>
2025-07-25 18:34:35 -0300

    Criação da tela Home (pt.1) *Novos aprendizados *Organização

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,6 +77,7 @@ dependencies {
     // Firebase Auth e Firestore
     implementation("com.google.firebase:firebase-auth-ktx")
     implementation("com.google.firebase:firebase-firestore-ktx")
+    implementation("androidx.activity:activity:1.8.0")
 
     // Testes
     testImplementation("junit:junit:4.13.2")

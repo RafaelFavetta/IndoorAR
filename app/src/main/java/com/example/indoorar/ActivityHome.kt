a6c1a895c3d90491a37091e56222fb1ea149df8c
Rafael Favetta <rafaelfavetta@gmail.com>
2025-08-18 17:11:11 -0300

    home teste para ver se o login funciona

---
+++ app/src/main/java/com/example/indoorar/ActivityHome.kt
@@ -0,0 +1,12 @@
+package com.example.indoorar
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ActivityHome : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}

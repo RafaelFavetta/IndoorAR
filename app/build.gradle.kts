d52c709a1f64a6efdb8afaa4eb01f13173a7aacd
baptistamiguel <miguel03baptista@gmail.com>
2025-08-26 22:30:02 -0300

    Tela de Perfil 0.1 -Nome -Email -Opções base

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ android {
 }
 
 dependencies {
+    implementation("com.google.android.material:material:1.12.0")
     // Core Android
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
@@ -95,6 +96,7 @@ dependencies {
 
     implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.activity:activity:1.8.0")
+    implementation("de.hdodenhof:circleimageview:3.1.0")
 
     testImplementation("junit:junit:4.13.2")
 }

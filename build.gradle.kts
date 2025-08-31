46cf370a588ebd61df466c6fab3ad90653b557bd
Rafael Favetta <rafaelfavetta@gmail.com>
2025-08-31 01:35:15 -0300

    sync pedindo que o AGP seja 8.9.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application") version "8.9.0" apply false
-    id("com.android.library") version "8.12.1" apply false
+    id("com.android.library") version "8.9.0" apply false
     kotlin("android") version "1.9.10" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
     id("com.google.gms.google-services") version "4.4.3" apply false

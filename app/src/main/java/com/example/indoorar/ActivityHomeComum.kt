e19d8f576359bb63cf9777b461a97b3d07b4a688
Rafael Favetta <rafaelfavetta@gmail.com>
2025-09-01 16:45:57 -0300

    redirecionando o botão de escanear

--- app/src/main/java/com/example/indoorar/ActivityHomeComum.kt
+++ app/src/main/java/com/example/indoorar/ActivityHomeComum.kt
@@ -1,20 +1,30 @@
 package com.example.indoorar
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.ImageView
 
 class ActivityHomeComum : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home_comum)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnEscanear = findViewById<ImageView>(R.id.btnEscanear)
+
+        btnEscanear.setOnClickListener {
+            val intent = Intent(this, ActivityScanQR::class.java)
+            startActivity(intent)
         }
     }
 }

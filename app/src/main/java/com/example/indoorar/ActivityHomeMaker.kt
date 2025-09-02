a8a30da459ac99cb99ed680d83c8d3419127ced7
Rafael Favetta <rafaelfavetta@gmail.com>
2025-09-02 17:34:48 -0300

    redirecionando o botao de 'Criar mapa' para o MapCreator

--- app/src/main/java/com/example/indoorar/ActivityHomeMaker.kt
+++ app/src/main/java/com/example/indoorar/ActivityHomeMaker.kt
@@ -1,20 +1,31 @@
 package com.example.indoorar
 
+import com.example.indoorar.ui.ActivityMapCreator
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.ImageView
 
 class ActivityHomeMaker : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home_maker)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Pegando o botão "Criar mapa"
+        val btnCriarMapa = findViewById<ImageView>(R.id.btnCriarMapa)
+        btnCriarMapa.setOnClickListener {
+            val intent = Intent(this, ActivityMapCreator::class.java)
+            startActivity(intent)
         }
     }
 }

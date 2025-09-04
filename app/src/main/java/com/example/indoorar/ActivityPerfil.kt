d1b6a7466f0d6b44f85808c96f0c88d5a2bce06b
Rafael Favetta <rafaelfavetta@gmail.com>
2025-09-04 17:26:12 -0300

    arrumando o findViewById

--- app/src/main/java/com/example/indoorar/ActivityPerfil.kt
+++ app/src/main/java/com/example/indoorar/ActivityPerfil.kt
@@ -12,10 +12,11 @@ class ActivityPerfil : BaseActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_perfil)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.constraintLayout)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
     }
-}
+    }

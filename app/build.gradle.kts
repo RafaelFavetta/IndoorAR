6dd64f8eefc61e3759e9caabac22a58c864c55f3
Rafael Favetta <rafaelfavetta@gmail.com>
2025-09-16 17:56:02 -0300

    implementando o gradle 8.14.3 e o JDK 20

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,17 +41,23 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_20
+        targetCompatibility = JavaVersion.VERSION_20
     }
 
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "20"
     }
 
     buildFeatures {
         viewBinding = true
         compose = true
+    }
+
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(20))
+        }
     }
 
     lint {

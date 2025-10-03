ffb8d78fcb6e8fae9b8dca8ba7d1de948f21e5d8
Rafael Favetta <rafaelfavetta@gmail.com>
2025-10-03 15:28:02 -0300

    renoeando a tela

--- app/src/main/java/com/example/indoorar/ActivityEditarNome.kt
+++ app/src/main/java/com/example/indoorar/ActivityEditarNome.kt
@@ -5,14 +5,13 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
-class ActivityAlterarNome : BaseActivity() {
+class ActivityEditarNome : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_alterar_nome)
+        setContentView(R.layout.activity_editar_nome)
 
         val btnVoltar = findViewById<ImageButton>(R.id.btnVoltar)
         btnVoltar.setOnClickListener { finish() }

b33d8af67e55b9cd903717743144444d696a9165
Rafael Favetta <rafaelfavetta@gmail.com>
2025-10-15 15:36:10 -0300

    mudanças básicas na tela de favoritos para nao deixá-la tão de lado

--- app/src/main/java/com/example/indoorar/ActivityFavoritos.kt
+++ app/src/main/java/com/example/indoorar/ActivityFavoritos.kt
@@ -3,10 +3,12 @@ package com.example.indoorar
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class ActivityFavoritos : AppCompatActivity() {
+class ActivityFavoritos : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favoritos)
     }
 }
+
+
 
